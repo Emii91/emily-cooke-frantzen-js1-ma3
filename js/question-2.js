bc00728f91ba83121e50b6140e7cf60a53fc0af8
Emii91 <69647535+Emii91@users.noreply.github.com>
2021-10-02 15:50:50 +0200

    qust 2

--- js/question-2.js
+++ js/question-2.js
@@ -3,19 +3,25 @@ const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const resultsContainer = document.querySelector(".results");
 
 async function getGames() {
-    try {
-        
-        const response = await fetch(url);
-        
-        const results = await response.json();
-        
-        console.log(results);
-    } catch (error) {
-        
-        console.log(error);
-        
-        resultsContainer.innerHTML = error
+
+    const response = await fetch(url);
+
+    const results = await response.json();
+
+    const facts = results.results;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < facts.length; i++) {
+        console.log(facts[i].name);
+
+        if (i === 8) {
+            break;
+        }
+
+        resultsContainer.innerHTML += `<div class="results">${facts[i].name}</div>`
     }
+
 }
 
 getGames();
